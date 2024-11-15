944234b604b93f93dff3bce3a7b38e86b8980dec
JoaoSalton <162753942+JoaoSalton@users.noreply.github.com>
2024-11-15 17:26:08 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,18 @@
 
 const CACHE_NAME = 'meu-cache-v1';
 const URLS_TO_CACHE = [
-    '/',                  // Página principal
-    '/index.html',        // HTML
-    '/styles.css',         // CSS
-    '/script.js',         // Seu JavaScript principal
-    '/area.jpg',          // Imagem de mapa JPG
-    '/cenoura.png',       // Ícone de waypoint
-    '/cavalo-up.png',     // Ícone de cavalo para direção para cima
-    '/cavalo-down.png',   // Ícone de cavalo para direção para baixo
-    '/cavalo-left.png',   // Ícone de cavalo para direção para a esquerda
-    '/cavalo-right.png',  // Ícone de cavalo para direção para a direita
-    '/horse.mp3',         // Som de passos
-    '/cenoura.mp3',       // Som de waypoint
-    '/corneta.mp3'        // Som de parabéns
+    './',                   // Página principal
+    './index.html',         // HTML
+    './styles.css',         // CSS
+    './script.js',          // Seu JavaScript principal
+    './area.jpg',           // Imagem de mapa JPG
+    './cenoura.png',        // Ícone de waypoint
+    './cavalo.png',         // Ícone de cavalo
+    './horse.mp3',          // Som de passos
+    './cenoura.mp3',        // Som de waypoint
+    './corneta.mp3'         // Som de parabéns
 ];
+
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
